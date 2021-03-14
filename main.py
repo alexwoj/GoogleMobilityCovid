488c528d5d376662c873fe46e85b8bc3b6c0fb05
alex.wojtowicz@gmail.com <alex.wojtowicz@gmail.com>
2021-03-13 22:55:55 -0300

    added get_date_range function

--- main.py
+++ main.py
@@ -13,4 +13,20 @@ gmr_rs = gmr_full[gmr_full['sub_region_1'] == 'State of Rio Grande do Sul']
 
 brio_full = pd.read_csv("/home/alex/PycharmProjects/GoogleMobilityCovid/data/caso_full.csv")
 brio_rs = brio_full[brio_full['state'] == 'RS']
+
+
+###########################
+# dataset information     #
+###########################
+
+gmr_rs.info()
 brio_rs.info()
+
+def get_date_range():
+    'Google mobility report dataset starts in 2020-02-15. We have to match these dates with Brasil.IO dataset.'
+
+    date_range = gmr_rs['date'].unique()
+    return date_range
+
+date_range = get_date_range()
+
